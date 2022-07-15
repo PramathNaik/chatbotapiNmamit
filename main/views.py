191f3c6c893c1d2e0e691cc1d34ce29e8de8584f
pramath <pramathbnaik@gmail.com>
2022-07-15 10:38:35 +0530

    messages

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ import random
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from feedback.models import feedback
-
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -11,6 +11,7 @@ def index(request):
         email = request.POST['email']
         feedbacks = request.POST['feedback']
         feedback.objects.create(name=name,email=email,feedback=feedbacks)
+        messages.add_message(request, messages.SUCCESS, 'Thank You for Your Response')
     return render(request,"bot.html")
 
 
