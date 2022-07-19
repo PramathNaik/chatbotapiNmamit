55823d9f8f85161398c85abebf7c602f26f8c496
pramath <pramathbnaik@gmail.com>
2022-07-19 14:50:13 +0530

    time setting complete

--- staticfiles/main.js
+++ staticfiles/main.js
@@ -86,16 +86,11 @@ $(window).on('load', function () {
 	msg.style.display = "none"
 });
 
-// Get the input field
 var input = document.getElementById("msg_input");
 
-// Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("send_button").click();
   }
 });
